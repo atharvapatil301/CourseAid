135e59b3398470757349e708c5b2098a0a1192cb
Atharva Patil <atharvp@users.noreply.huggingface.co>
2026-01-02 06:49:33 +0000

    Update app/__init__.py

--- app/__init__.py
+++ app/__init__.py
@@ -1,25 +1,34 @@
 import os
 from flask import Flask
 from flask_cors import CORS
-from .middleware.auth import auth
-from .config import db_connection
+from werkzeug.middleware.proxy_fix import ProxyFix
 from dotenv import load_dotenv
 
 load_dotenv()
 
-token = os.getenv("HF_TOKEN")
-
-from huggingface_hub import login
-login(token=token)
-
-app = Flask(__name__,
-            template_folder='./view/templates',
-            static_folder='./view/static')
+app = Flask(
+    __name__,
+    template_folder="./view/templates",
+    static_folder="./view/static"
+)
 
 app.secret_key = os.getenv("SECRET_KEY")
-app.register_blueprint(auth)
-CORS(app)
 
-conn = db_connection.connect()
+app.config.update(
+    SESSION_COOKIE_SECURE=True,
+    SESSION_COOKIE_HTTPONLY=True,
+    SESSION_COOKIE_SAMESITE="None",
+)
+
+CORS(
+    app,
+    supports_credentials=True,
+    origins=["https://*.hf.space"]
+)
+
+app.wsgi_app = ProxyFix(app.wsgi_app, x_proto=1, x_host=1)
+
+from .middleware.auth import auth
+app.register_blueprint(auth)
 
 from .routes import api_routes
